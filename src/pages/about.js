246e409d20a2e174ddada6f932703e4e03481aaa
kutec <ku@kutec.co.in>
2019-11-16 22:30:36 +0530

    Added sass and post css

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Header from "../components/header"
+import Container from "../components/container"
 
 export default () => (
-  <div style={{ color: `teal` }}>
+  <Container>
     <Header headerText="About Gatsby" />
     <Header headerText="It's pretty cool" />
     <p>Such wow. Very React.</p>
-  </div>
+  </Container>
 )
