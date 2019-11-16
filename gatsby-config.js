da50828fce2263c8284f196817f453207e1a678e
kutec <ku@kutec.co.in>
2019-11-16 23:16:56 +0530

    Added Layout and Typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,11 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
   ]
 }
